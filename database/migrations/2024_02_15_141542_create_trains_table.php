c4c1e3f4d6060b3e66d3c9fbcdcdbf47f3283be9
aletardio <ale.tardio99@gmail.com>
2024-02-15 15:49:06 +0100

    Created table trains

---
+++ database/migrations/2024_02_15_141542_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->smallInteger('code_train')->unisgned();
+            $table->tinyInteger('number_coaches')->unsigned();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('delete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
