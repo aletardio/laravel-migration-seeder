976caf225e6f1982759037cc7a6dab029e2dc558
aletardio <ale.tardio99@gmail.com>
2024-02-15 17:54:50 +0100

    Styled button on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,19 @@
 @section('content')
     <div class="centered-button">
         <h1 class="text-center">Booltrain</h1>
-        <a href="{{ route('index_train') }}" class="btn btn-sm btn-primary d-flex justify-content-center">Visualizza i
-            treni</a>
+        <button class="animated-button d-flex justify-content-center">
+            <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+                </path>
+            </svg>
+            <a href="{{ route('index_train') }}" class="text text-black">Visualizza i treni</a>
+            <span class="circle"></span>
+            <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+                </path>
+            </svg>
+        </button>
     </div>
 @endsection
